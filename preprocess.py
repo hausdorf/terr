de4ea002e67680531ae025b6bca8826ea1382ac9
vishayv <vishay.vanjani@gmail.com>
2011-10-30 23:38:12 -0600

    updated preprocess to create sentence chunks and generates separate files for meta and main texts e.g DEV-MUC-0006 divided into meta_DEV-MUC-0006 and main_DEV-MUC-0006

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,5 @@
 import os, re, stat
-
+import nltk
 
 # WHERE YOU PUT OUR DATA
 PATH = 'developset/texts/'  # Old data
@@ -10,6 +10,7 @@ ANY_TAG = '\[[\s\S]+?\]' # Finds any thing inside brackets, e.g. [TEXT]
 TEXT = '\[[TEXT]+\]'   # Everything before the [TEXT] tag
 START_TAG= '\[TEXT\]|\[EXCERPT\]|\[EXCERPTS\]' # SELECT MAIN TAG 
 FIRST =".+?\n" # selects first line of each text 
+REDUNDANT_NEWLINE = "\n(?=.)" # selects a new line if it occurs before some char
 
 # Helper: iterates through file by line, then closes file
 def f_iter(filename):
@@ -33,21 +34,6 @@ def f_write(filename, text):
 	f.write(text)
 	f.close()
 
-def remove_first_line(text):
-	text = re.sub(FIRST,"",text,1)
-	return text
-
-# splits the input text into meta_info and main_text,squishes brackets in main
-# and returns a join separated by 3 lines 
-def split_text(text):
-	text_arr  = re.split(START_TAG,text,1)
-	if(len(text_arr) != 2):
-		return -1 
-	else:
-		#remove any tags in main_text 
-		text_arr[1] = re.sub(ANY_TAG,"",text_arr[1])
-		return "\n\n\n".join(text_arr)
-	 
 # Removes everything before [TEXT] if such a tag is present
 def remove_if_tscript(text):
 	m = re.search(TEXT, text)
@@ -73,29 +59,55 @@ def remove_if_brackets(text):
 
 	return text
 
+# removes first line 	
+def remove_first_line(text):
+	text = re.sub(FIRST,"",text,1)
+	return text
+
+# splits the input text into meta_info and main_text,squishes brackets in main
+# and returns a tuple
+def split_text(text):
+	text_arr  = re.split(START_TAG,text,1)
+	if(len(text_arr) != 2):
+		return (-1,-1) 
+	else:
+		# remove blank line if exists in meta text 
+		text_arr[0] = re.sub(REDUNDANT_NEWLINE,"",text_arr[0])
+		#remove any tags in main_text 
+		text_arr[1] = re.sub(ANY_TAG,"",text_arr[1])
+		return (text_arr[0],text_arr[1])
+	 
+# split into sentence 
+def sent_splitter(text):
+	sent_tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer() 
+	sent_list = sent_tokenizer.tokenize(text)
+	for i in range(len(sent_list)):
+		# remove \n in the middle of the line if it exists 
+		sent_list[i] = re.sub(REDUNDANT_NEWLINE," ",sent_list[i])
+	return "\n".join(sent_list) 
 
 # Preprocess that file!
 def pprocess_file(filename):
 	text = f_read(filename)
 	text = remove_first_line(text) 
-	ret = split_text(text)
-	if(ret == -1):
+	(meta,main) = split_text(text)
+	if(meta == -1 ):
 		print "error splitting file",filename,"\n"
-	else:
-		text = ret 
 
-#text = remove_if_head(text)
-#text = remove_if_brackets(text)
-
-	return text
+	return (meta,main)
 
 
 def main():
 	for root, dirs, files in os.walk(PATH):
 		for file in files:
-			text = pprocess_file(root + file)
-			f_write(REPL_DIR + file, text)
-
+			(meta,main) = pprocess_file(root + file)
+			meta_tag = "meta_" 
+			main_tag = "main_"
+			f_write(REPL_DIR + meta_tag+file, meta)
+			# send main to sent splitter 
+			main_text = sent_splitter(main)
+			f_write(REPL_DIR + main_tag+file, main_text)
+			
 
 if __name__ == '__main__':
 	main()
