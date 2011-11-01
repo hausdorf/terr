35460fccb1f697a82076a16202a84f64af474f44
vishayv <vishay.vanjani@gmail.com>
2011-10-31 19:55:36 -0600

    All the parsed files are now written to *_parsed directory/  and have a .parsed extension

--- java_parser/Parse.java
+++ java_parser/Parse.java
@@ -4,6 +4,7 @@ import java.io.File ;
 import java.io.FilenameFilter;
 import java.io.FileFilter;
 import java.io.BufferedWriter;
+import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.util.*;
 
@@ -19,6 +20,7 @@ import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
 class Parse {
 
 
+	  static boolean DEBUG = false ;
 	  static String curr_parser = "englishFactored.ser.gz";
 	  static String backup_parser = "englishPCFG.ser.gz";
 	  static String out_prefix =".parsed";
@@ -40,7 +42,7 @@ class Parse {
 					}
 
 	  }
-	  public static File[] applyFileFilter(File files[]){
+	  public static File[] applyFileFilter(File files[]){ // everything is passed by reference in java
 
 			  FileFilter fileFilter = new FileFilter() {
 				      public boolean accept(File file) {
@@ -48,7 +50,7 @@ class Parse {
 								      }
 			  };
 			  files = dir.listFiles(fileFilter);
-			  return files ;
+			  return files ; // have to return because we are changing the place the reference points too
 	  }	  
 	  public static File[] getFiles(){
 
@@ -59,10 +61,8 @@ class Parse {
 					  System.exit(1);
 
 			  }	  
-			  System.out.println("check for length of original children arr "+files.length);
 			  files = applyFileFilter(files);
-			  System.out.println("check for length of new  children arr "+files.length);
-			  return files;
+			  return files;  
 	  }	  
 	  public static void main(String[] args) throws IOException {
 
@@ -71,8 +71,6 @@ class Parse {
 
 		      LexicalizedParser lp = new LexicalizedParser(curr_parser);
 			  LexicalizedParser lp_backup ; 
-		  	  // PARSE EACH LINE OF THE TEXT --options sentence input and other things
-			  //lp.setOptionFlags("-outputFormat","penn,oneline,typedDependenciesCollapsed,wordsAndTags","-MAX_ITEMS","600000","-maxLength", "80", "-retainTmpSubcategories");
 			  lp.setOptionFlags("-MAX_ITEMS","700000","-maxLength", "80", "-retainTmpSubcategories");
 			  TreebankLanguagePack tlp = new PennTreebankLanguagePack();
 			  GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
@@ -86,75 +84,64 @@ class Parse {
 					String file_path = file.getPath();
 			    	String new_abs_path = out_dir_path+filename + out_prefix ;
 					// open file handle for writing 
-					BufferedWriter out = new BufferedWriter(new FileWriter(new_abs_path));
-			  	  	System.out.println("processing file "+file_path);
-			  	  	System.out.println("processing filename "+filename);
-			  	  	System.out.println("processing filepath "+file_path);
-			  	  	System.out.println("output abs path "+new_abs_path);
+					if(DEBUG){
+						System.out.println("processing file "+file_path);
+						System.out.println("processing filename "+filename);
+						System.out.println("processing filepath "+file_path);
+						System.out.println("output abs path "+new_abs_path);
+					}
 					if(filename.startsWith(".")){
-						  System.out.println("skipping  processing for file "+filename);
+						  if(DEBUG){
+						  	System.out.println("skipping  processing for file "+filename);
+						  }
 						  continue ;
 					}	  
 					if (filename.startsWith("meta")){
-						  System.out.println("skipping  processing for file "+filename);
+						  if(DEBUG){
+						  	System.out.println("skipping  processing for file "+filename);
+						  }
 						  continue ;
 					}
+					PrintWriter out = new PrintWriter(new FileWriter(new_abs_path));
 			  	  	Iterable< List<? extends HasWord> > sentences;
 			  		DocumentPreprocessor dp = new DocumentPreprocessor(file_abs_path);
-      		  		//DocumentPreprocessor dp = new DocumentPreprocessor("test.txt");
       		  		List<List<? extends HasWord>> tmp = new ArrayList<List<? extends HasWord>>();
       		  		for (List<HasWord> sentence : dp) {
         				tmp.add(sentence);
       		  		}
       		  		sentences = tmp;
 			  		for (List<? extends HasWord> sentence : sentences) {
-						System.out.println("Processing sentence "+sentence);
+						if(DEBUG){
+							System.out.println("Processing sentence "+sentence);
+						}
       					Tree parse = lp.apply(sentence);
 						int depth = parse.depth();
 
-						System.out.println("depth of tree"+depth);
+						if(DEBUG){
+							System.out.println("depth of tree"+depth);
+						}
 						if(depth == 2){
 								//parse = null ;
 								lp = null ;
 		      				    lp_backup = new LexicalizedParser(backup_parser);
 			  					lp_backup.setOptionFlags("-MAX_ITEMS","700000","-maxLength", "80", "-retainTmpSubcategories");
 								parse = lp_backup.apply(sentence);
-    							out.write(parse.pennString());
+    							//out.write(parse.pennString());
 								lp_backup = null ;
 								lp = new LexicalizedParser(curr_parser);
 			  					lp.setOptionFlags("-MAX_ITEMS","700000","-maxLength", "80", "-retainTmpSubcategories");
 								// now extract info from backup
 						}	
-				    	//parse.pennPrint();
-				  		//System.out.println();
-				  		//System.out.println(parse.taggedYield());
-    					out.write(parse.pennString());
-				  		//System.out.println();
-						/*FOR Printing dependency in a line*/ 
-				 		//GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
-						//Collection tdl = gs.typedDependenciesCCprocessed(true);
-						// out putting the typed dependency 
-						//System.out.println(tdl);
-				  		//System.out.println();
-						TreePrint tp = new TreePrint("oneline,typedDependenciesCollapsed");
-			      		//tp.printTree(parse);
-						//out.write(tp.printTree(parse));
-						//System.out.println();
+						TreePrint tp = new TreePrint("wordsAndTags,oneline,typedDependenciesCollapsed");
+						tp.setPrintWriter(out);
+			      		tp.printTree(parse);
+						out.write("\n");
 
 					}
-				  	/*List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
-				  	System.out.println(tdl);
-				  	System.out.println();
-				  	TreePrint tp = new TreePrint("penn,typedDependenciesCollapsed");
-			      	tp.printTree(parse);
-				  	*/
     				out.close();
-										// now write 
 				}
 
 
-			    // WRITE FILE IN PARSEDTEXTS directory
-				// CREATE A DIRECTORY 	
 				  
   }
 
