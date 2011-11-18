d61ef897db3864759fe2d2b56ff227b2e1eb2159
Alex Clemmer <clemmer.alexander@gmail.com>
2011-11-18 15:41:15 -0700

    Add matching dict that aggregates counts of matched patterns

--- matching.py
+++ matching.py
@@ -1,16 +1,6 @@
 from aslog import groups
 import collections
 
-PATT_WEAP = ['<np> exploded']
-PATT_PIND = ['<np> kidnapped']
-PATT_PORG = []
-PATT_TARG = ['<np> exploded', 'attack on <np>', 'exploded in <np>',
-		'occurred on', 'destroyed <np>', 'exploded on <np>']
-PATT_VICT = ['murder of <np>', 'assassination of <np>', '<np> was killed',
-		'<np> was kidnapped', 'attack on <np>', '<np> was injured',
-		'death of <np>', 'claimed <np>', '<np> was wounded',
-		'destroyed <np>', '<np> was murdered', '<np> died']
-
 
 def fmt_prev(l):
 	grp = list(groups(l, key=lambda x:x.split('/')[1]))
@@ -30,73 +20,26 @@ def find_thing(prsed, patterns):
 		j = 0
 		while i < lprsedspl and j < lpattspl:
 			curr = prsedspl[i].split('/')[0]
-			if pattspl[j] == '<NP>' or curr == pattspl[j]:
+			if pattspl[j] == '<VICTIM>' or curr == pattspl[j]:
 				j += 1
 			else:
 				i -= j
 				j = 0
 
 			if j == lpattspl:
-				yield fmt_prev(prsedspl[:i]), patt
+				#yield fmt_prev(prsedspl[:i]), patt
+				yield patt
 				j = 0
 				i -= 1
 			i += 1
 
-def agg_weapon(prsed):
-	found = list(find_thing(prsed, PATT_WEAP))
-	if len(found) == 0:
-		return '-'
-	return found
+def aggregate(prsed, patts):
+	cnts = collections.defaultdict(lambda: 0)
+	for l in prsed:
+		for t in find_thing(l, patts):
+			cnts[t] += 1
 
-def agg_perp_indiv(prsed):
-	found = list(find_thing(prsed, PATT_PIND))
-	if len(found) == 0:
-		return '-'
-	return found
-
-def agg_perp_org(prsed):
-	found = list(find_thing(prsed, PATT_PORG))
-	if len(found) == 0:
-		return '-'
-	return found
-
-def agg_target(prsed):
-	found = list(find_thing(prsed, PATT_TARG))
-	if len(found) == 0:
-		return '-'
-	return found
-
-def agg_victim(prsed):
-	found = list(find_thing(prsed, PATT_VICT))
-	if len(found) == 0:
-		return '-'
-	return found
-
-def tally_rslts(d, rslts, slot):
-	if rslts == '-':
-		return
-
-	for chnk, rslt in rslts:
-		d[slot][rslt] += 1
-
-def aggregate(prsed):
-	
-	result = {
-		'INCIDENT': collections.defaultdict(lambda:0),
-		'WEAPON': collections.defaultdict(lambda:0),
-		'PERP INDIV': collections.defaultdict(lambda:0),
-		'PERP ORG': collections.defaultdict(lambda:0),
-		'TARGET': collections.defaultdict(lambda:0),
-		'VICTIM': collections.defaultdict(lambda:0)
-		}
-
-	tally_rslts(result, agg_weapon(prsed), 'WEAPON')
-	tally_rslts(result, agg_perp_indiv(prsed), 'PERP INDIV')
-	tally_rslts(result, agg_perp_org(prsed), 'PERP ORG')
-	tally_rslts(result, agg_target(prsed), 'TARGET')
-	tally_rslts(result, agg_victim(prsed), 'VICTIM')
-
-	return result
+	return cnts
 
 def p(d):
 	for slot,d1 in d.items():
