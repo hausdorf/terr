0c7af4a49730af0385f430ff6e52d77b1ab1b84f
Alex Clemmer <clemmer.alexander@gmail.com>
2011-11-20 15:37:27 -0700

    Add metadata-processing

---
+++ meta.py
@@ -0,0 +1,28 @@
+from __future__ import with_statement
+
+
+def find_communique(data):
+	ldata = len(data)
+	rslt = []
+
+	for i in range(ldata):
+		if data[i] == '[COMMUNIQUE]':
+			while i+1 < ldata:
+				curr = data[i+1]
+				print curr
+				rslt.append(curr)
+				if curr.count(']') > 0:
+					return ' '.join(rslt)
+				i += 1
+
+	return None
+
+def proc_meta(data):
+	sp = data.split()
+
+	return find_communique(sp)
+
+
+if __name__ == '__main__':
+	with open('sample-textfile.txt') as f:
+		proc_meta(f.read())
